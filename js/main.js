b3da3bef967e08233d4fc74f23d8b685f9e7fbd3
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-09-09 12:26:06 -0300

    Add arquivos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function imgSlider(todos) {
+    document.querySelector('.pepsi').src = todos;
+}
+
+function mudarBgColor(cor) {
+    const section = document.querySelector('#bgColor');
+    section.style.background = cor;
+}
